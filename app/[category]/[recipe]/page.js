969bee90d69e91261c4541814f6ab882dc4216a6
nhnahid1122 <mdxx0356@stud.kea.dk>
2025-02-07 20:40:11 +0100

    fix the qutaion and apos error

--- app/[category]/[recipe]/page.js
+++ app/[category]/[recipe]/page.js
@@ -84,7 +84,7 @@ export default function RecipeDetails({ params }) {
 
         <h2 class="text-3xl font-bold mb-4">Here are the basics</h2>
         <p class="mb-8">
-          Juicy meatballs brisket slammin' baked shoulder. Juicy smoker soy
+          Juicy meatballs brisket slammin&apos; baked shoulder. Juicy smoker soy
           sauce burgers brisket. polenta mustard hunk greens. Wine technique
           snack skewers chuck excess. Oil heat slowly. slices natural delicious,
           set aside magic tbsp skillet, bay leaves brown centerpiece. fruit
@@ -93,11 +93,11 @@ export default function RecipeDetails({ params }) {
         </p>
 
         <blockquote class="text-3xl font-bold italic text-center my-12 px-4">
-          "One cannot think well, love well, sleep well, if one has not dined
-          well."
+        &quot;One cannot think well, love well, sleep well, if one has not dined
+          well.&quot;
         </blockquote>
         <p class="text-center text-gray-600 mb-12">
-          — Virginia Woolf, A Room of One's Own
+          — Virginia Woolf, A Room of One&apos;s Own
         </p>
 
         <h2 class="text-3xl font-bold mb-4">In the kitchen</h2>
@@ -118,7 +118,7 @@ export default function RecipeDetails({ params }) {
         />
 
         <p class="mb-8">
-          Juicy meatballs brisket slammin' baked shoulder. Juicy smoker soy
+          Juicy meatballs brisket slammin&apos; baked shoulder. Juicy smoker soy
           sauce burgers brisket. polenta mustard hunk greens. Wine technique
           snack skewers chuck excess. Oil heat slowly. slices natural delicious,
           set aside magic tbsp skillet, bay leaves brown centerpiece. fruit
